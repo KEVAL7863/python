e2d45f8a7dd9bc9aeca04dad96546588852611b3
Keval Gelani <kevalgelani7863@gmail.com>
2026-02-12 08:34:07 +0530

    updated file

--- main.py
+++ main.py
@@ -766,13 +766,13 @@
 # print(sec_largest, largest)
 
 
-a = [12,13,14,15,16]
+# a = [12,13,14,15,16]
 
-for i in range(len(a)-1):
-    if a[i] < a[i+1]:
-        continue
-    else:
-        print("your list is not sorted")
-        break
-else:
-    print("your list is sorted")    
+# for i in range(len(a)-1):
+#     if a[i] < a[i+1]:
+#         continue
+#     else:
+#         print("your list is not sorted")
+#         break
+# else:
+#     print("your list is sorted")    
